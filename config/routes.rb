555e8b4f51eeb10c16232291a2058b1a6a27b0f0
shin <shin44240@gmail.com>
2017-02-26 21:53:55 +0900

    plansコントローラとindex,showのビューを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'plans/index'
+
+  get 'plans/show'
+
+  root to: "plans#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
